2a02c6a61ddfa9678c6279b74b00d0df1df447ad
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-03-04 04:12:21 +0600

    card design primarily okay

--- script/index.js
+++ script/index.js
@@ -4,6 +4,27 @@ const loadLessons = () => {
   .then(json => displayLessons(json.data));
 };
 
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById('word-container');
+  // wordContainer.innerHTML = "";
+
+  words.forEach(word => {
+    console.log(word);
+    const card = document.createElement('div');
+    card.innerHTML = `
+        <p>cat</p>
+    `;
+    wordContainer.append(card);
+  });
+};
+
 displayLessons = (lessons) => {
   // step 1: get the container
   const levelContainer = document.getElementById('level-container');
@@ -13,7 +34,7 @@ displayLessons = (lessons) => {
     // step 3: create element
     const btnDiv = document.createElement('div');
     btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no} </button>
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no} </button>
     `
     // step 4: append into container
     levelContainer.append(btnDiv);
