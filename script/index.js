315169f05f6495eb3193c29e0cc4fd27c1b2a8ae
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-03-04 05:57:01 +0600

    all cards and vocabs dynamically loaded

--- script/index.js
+++ script/index.js
@@ -1,25 +1,34 @@
 const loadLessons = () => {
   fetch('https://openapi.programming-hero.com/api/levels/all')
-  .then(res => res.json())
-  .then(json => displayLessons(json.data));
+    .then(res => res.json())
+    .then(json => displayLessons(json.data));
 };
 
 const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => displayLevelWord(data.data));
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data));
 };
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById('word-container');
-  // wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
 
   words.forEach(word => {
     console.log(word);
     const card = document.createElement('div');
     card.innerHTML = `
-        <p>cat</p>
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+          <h2 class="font-bold text-2xl">${word.word}</h2>
+          <p class="font-semibold">Meaning / Pronunciation</p>
+          
+          <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+          <div class="flex justify-between items-center">
+              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>  
+          </div>
+       </div>
     `;
     wordContainer.append(card);
   });
@@ -39,6 +48,6 @@ displayLessons = (lessons) => {
     // step 4: append into container
     levelContainer.append(btnDiv);
   }
-  
+
 };
 loadLessons();
