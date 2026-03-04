9d515d0e2b59a57e89ff011346f9fbedb6d2b011
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-03-04 07:23:55 +0600

    modal added

--- script/index.js
+++ script/index.js
@@ -47,7 +47,7 @@ const displayLevelWord = (words) => {
           
           <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</div>
           <div class="flex justify-between items-center">
-              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+              <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
               <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>  
           </div>
        </div>
